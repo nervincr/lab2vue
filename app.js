266b12a09b3d8ce8c2b1afe75b5069b464130fe9
nervincr <nervincr@users.noreply.github.com>
2019-08-27 00:41:00 -0600

    Crud listo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var BaseApiUrl = "http://localhost/Lab2/index.php/api/news/";
+var BaseApiUrl = "http://localhost:8080/Lab2/index.php/api/news/";
 
 function buildUrl(service){
     return BaseApiUrl + service;
@@ -9,6 +9,11 @@ window.onload = function(){
         el: '#app',
         data: {
           fields: ['id','title','text', 'actions'],
+          noti: {
+              id: 0,
+              title: '',
+              text: ''
+          },
           news: []
         },
         mounted(){
@@ -20,11 +25,56 @@ window.onload = function(){
                     this.news = response.data;
                 }).catch(error => {console.log(error)});
             },
+            submitData(evt){
+                evt.preventDefault();
+                if (this.noti.id > 0) {
+                    this.editData();
+                } else {
+                    this.addData();                    
+                }
+                this.noti.id = 0;
+                this.noti.title = "";
+                this.noti.text = "";
+            },
             deleteData(id){
                 axios.delete(buildUrl('news/' + id)).then((response) => {
                     this.getData();
+                    toastr.success("La noticia fue eliminada con éxito");
                 }).catch(error => {console.log(error)});
-            }   
+            },
+            addData(){
+                axios.post(
+                    buildUrl('news'), 
+                    {
+                        id: this.noti.id,
+                        title: this.noti.title,
+                        text: this.noti.text
+                    }
+                ).then((response) => {
+                    this.getData();
+                    toastr.success("La noticia fue agregada con éxito");
+                }).catch(error => {console.log(error)});
+            },
+            editData(){
+                axios.put(
+                    buildUrl('news'), 
+                    {
+                        id: this.noti.id,
+                        title: this.noti.title,
+                        text: this.noti.text
+                    }
+                ).then((response) => {
+                    this.getData();
+                    toastr.success("La noticia fue actualizada con éxito");
+                }).catch(error => {console.log(error)});
+            }, 
+            getEditData(id){
+                axios.get(buildUrl('news/' + id)).then((response) => {
+                    this.noti.id = response.data.id;
+                    this.noti.title = response.data.title;
+                    this.noti.text = response.data.text;
+                }).catch(error => {console.log(error)});
+            },
         }
       });
 }
