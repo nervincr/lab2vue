2f87104761a5a6993b09cfb0dbd643a9e631edb6
nervincr <nervincr@hotmail.com>
2019-08-28 20:52:53 -0600

    publicado y APK

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var BaseApiUrl = "http://localhost:8080/Lab2/index.php/api/news/";
+var BaseApiUrl = "http://nervinlicweb.000webhostapp.com/lab3crud/index.php/api/news/";
+var ws;
 
 function buildUrl(service){
     return BaseApiUrl + service;
 }
 
 window.onload = function(){
+    document.addEventListener("deviceready", onDeviceReady, false);
     var vm = new Vue({
         el: '#app',
         data: {
@@ -12,16 +14,19 @@ window.onload = function(){
           noti: {
               id: 0,
               title: '',
-              text: ''
+              text: '',
+              slug: '',
+              image: ''
           },
           news: []
         },
         mounted(){
             this.getData();
+            this.MyWebSocketCall();
         },
         methods:{
             getData(){
-                axios.get(buildUrl('news')).then((response) => {
+                axios.get(buildUrl('getnews')).then((response) => {
                     this.news = response.data;
                 }).catch(error => {console.log(error)});
             },
@@ -35,20 +40,25 @@ window.onload = function(){
                 this.noti.id = 0;
                 this.noti.title = "";
                 this.noti.text = "";
+                this.noti.slug = "";
+                this.noti.image = "";
             },
             deleteData(id){
-                axios.delete(buildUrl('news/' + id)).then((response) => {
+                axios.get(buildUrl('deletenews/' + id)).then((response) => {
+                    ws.send("delete");
                     this.getData();
                     toastr.success("La noticia fue eliminada con éxito");
                 }).catch(error => {console.log(error)});
             },
             addData(){
                 axios.post(
-                    buildUrl('news'), 
+                    buildUrl('insertnews'), 
                     {
                         id: this.noti.id,
                         title: this.noti.title,
-                        text: this.noti.text
+                        slug: this.noti.slug,
+                        text: this.noti.text,
+                        image: this.noti.image
                     }
                 ).then((response) => {
                     this.getData();
@@ -56,12 +66,14 @@ window.onload = function(){
                 }).catch(error => {console.log(error)});
             },
             editData(){
-                axios.put(
-                    buildUrl('news'), 
+                axios.post(
+                    buildUrl('updateNews'), 
                     {
                         id: this.noti.id,
                         title: this.noti.title,
-                        text: this.noti.text
+                        slug: this.noti.slug,
+                        text: this.noti.text,
+                        image: this.image
                     }
                 ).then((response) => {
                     this.getData();
@@ -75,6 +87,53 @@ window.onload = function(){
                     this.noti.text = response.data.text;
                 }).catch(error => {console.log(error)});
             },
+            MyWebSocketCall() {
+                if ("WebSocket" in window) {
+                    console.log("WebSocket is supported by your Browser!");
+                    // Let us open a web socket
+                    //personalizamos la url con nuestro propio room_id
+                    //wss://connect.websocket.in/YOUR_CHANNEL_ID?room_id=YOUR_ROOM_ID
+                    ws = new WebSocket("wss://connect.websocket.in/nervinlicweb?room_id=9999");
+                    ws.onopen = function() {
+                    // Web Socket is connected, send data using send()
+                    ws.send("open");
+                    console.log("WebSocket is open...");
+                    };
+                    ws.onmessage = function (evt) {
+                    //cada vez que se invoca el ws.send() se recibe una respuesta de forma asincrónica
+                    vm.getData();
+                    console.log("Message is received: " + evt.data); //evt.data contiene el msj recibido
+                    };
+                    ws.onclose = function() {
+                    // websocket is closed.
+                    console.log("Connection is closed...");
+                    };
+                } else {
+                    // The browser doesn't support WebSocket
+                    alert("WebSocket NOT supported by your Browser!");
+                }
+            }
         }
       });
+}
+function onDeviceReady() {
+    // Register the event listener
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+function onConfirm(buttonIndex) {
+    if (buttonIndex==2){
+        navigator.app.exitApp();
+    }
+}
+
+// Handle the back button
+//
+function onBackKeyDown() {
+    navigator.notification.confirm(
+        '¿Está seguro que desea salir?',  // message
+        onConfirm,              // callback to invoke with index of button pressed
+        'Salir de myApp',            // title
+        'No,Sí'         // buttonLabels
+    );
 }
